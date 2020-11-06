83fabc09d741de9d75d876a3a2b20c79aeb108bd
raaynaldo <raaynaldo@hotmail.com>
2020-11-06 15:55:16 -0500

    UI

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,25 @@
 class CompaniesController < ApplicationController
+    def index
+        @companies = Company.all
+    end
+
+    def show
+        @company = Company.find(params[:id])
+    end
+
+    def new
+        @company = Company.new
+    end
+    
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,25 @@
 class TeachersController < ApplicationController
+    def index
+        @teachers = Teacher.all
+    end
+
+    def show
+        @teacher = Teacher.find(params[:id])
+    end
+
+    def new
+        @teacher = Teacher.new
+    end
+    
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
