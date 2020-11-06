a5972b4abdae1848aba97dc619ecc577b99a0503
raaynaldo <raaynaldo@hotmail.com>
2020-11-06 15:19:57 -0500

    coursess view index, new, show

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :students
   resources :courses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: "static#home"
+  get '/about', to: "static#about"
 end
