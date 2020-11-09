51d4faa414bde45437e99c887878bc1b83cef76c
chirptotheeast <alyssa.easterly@yahoo.com>
2020-11-09 15:26:20 -0600

    added validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,6 @@ class Student < ApplicationRecord
     has_secure_password
     has_many :course_registrations
     has_many :courses, through: :course_registrations
+
+    validates :email, presence: true, uniqueness: true
 end
