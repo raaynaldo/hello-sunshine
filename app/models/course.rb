d1ddbc0e89e19467c3b15acd93c295c9962c60ce
chirptotheeast <alyssa.easterly@yahoo.com>
2020-11-10 16:40:15 -0600

    working on new courses form

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,8 @@ class Course < ApplicationRecord
     has_many :students, through: :course_registrations
     belongs_to :teacher
     
+    validates :title, presence: true
+    validates :
     def date_to_s
         self.date.strftime("%B %d, %Y")
     end
