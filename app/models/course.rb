7247cf76fb98ebede0f996013e1479673b6c9fd5
chirptotheeast <alyssa.easterly@yahoo.com>
2020-11-11 14:11:56 -0600

    updated validations and teacher index work

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,11 @@ class Course < ApplicationRecord
     belongs_to :teacher
     
     validates :title, presence: true
-    
+    validates :price, presence: true
+    validates :min_age, numericality: {greater_than: -1, less_than_or_equal_to: :max_age }, presence: true
+    validates :max_age, numericality: {less_than_or_equal_to: 14 }, presence: true
+    validates :max_student, numericality: {less_than_or_equal_to: 15}
+
     def date_to_s
         self.date.strftime("%B %d, %Y")
     end
